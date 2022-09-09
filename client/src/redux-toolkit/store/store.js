e230a966daf55f25dc2a9be60b9838fa7d16c17b
Rasaf Ibrahim <rasaf.ibrahim25@gmail.com>
2022-09-09 22:51:33 +0600

    rtk query bookmarkApi sertup finished

--- client/src/redux-toolkit/store/store.js
+++ client/src/redux-toolkit/store/store.js
@@ -3,17 +3,21 @@ import { configureStore } from '@reduxjs/toolkit'
 
 // MovieSearch
 import { MovieSearchApi } from 'redux-toolkit/api/movie-api'
-
+import {BookmarkApi} from 'redux-toolkit/api/bookmark-api'
 
 
 export const store = configureStore({
 
   reducer: {
-    [MovieSearchApi.reducerPath]: MovieSearchApi.reducer
+    [MovieSearchApi.reducerPath]: MovieSearchApi.reducer,
+
+    [BookmarkApi.reducerPath]: BookmarkApi.reducer
   },
 
+
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(MovieSearchApi.middleware)
+    
+    getDefaultMiddleware().concat(MovieSearchApi.middleware).concat(BookmarkApi.middleware)
 
 
 })
