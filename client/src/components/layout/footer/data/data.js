432529da3f98c6becf3d037cf00ae1d945ca0dc1
Rasaf Ibrahim <rasaf.ibrahim25@gmail.com>
2023-05-14 14:29:43 +0600

    (frontend) updated footer

--- client/src/components/layout/footer/data/data.js
+++ client/src/components/layout/footer/data/data.js
@@ -1,10 +1,74 @@
 import LOGO___COMPONENT from '@/components/reusable/just-for-this-project/logo/logo';
-import { Facebook, Twitter, Instagram, LinkedIn } from '@mui/icons-material';
+import Facebook from "@mui/icons-material/Facebook";
+import Twitter from "@mui/icons-material/Twitter";
+import Instagram from "@mui/icons-material/Instagram";
+import LinkedIn from "@mui/icons-material/LinkedIn";
+import GitHub  from '@mui/icons-material/Github';
 
 
 
 
 export const footer_data = {
+
+
+    /* 🍪 */
+    basic: {
+
+        logo_component: LOGO___COMPONENT,
+
+        about: 'Search any movie, know details info of a movie, bookmark your favorite movie and more!',
+
+    },
+
+
+    /* 🍪 */
+    social_accounts: [
+        {
+            icon_component: Twitter,
+            link: 'https://twitter.com/_Rasaf_Ibrahim'
+        },
+
+        {
+            icon_component: LinkedIn,
+            link: 'https://www.linkedin.com/in/rasaf-ibrahim'
+        },
+
+        {
+            icon_component: GitHub,
+            link: 'https://github.com/Rasaf-Ibrahim'
+        },
+
+
+
+
+    ],
+
+
+
+
+    /* 🍪 */
+    groups_of_link: [
+
+
+        {
+            title: 'Legal',
+
+            links: [
+
+                {
+                    text: 'Privacy Policy',
+                    url: '/privacy-policy'
+                },
+
+                {
+                    text: 'Terms of Service',
+                    url: '/terms-of-service'
+                },
+
+            ],
+        },
+
+    ]
 
 
 }
