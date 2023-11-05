8e76e711bfe774e24fa745a9cec4c7a397513a68
Rasaf-Ibrahim <rasaf.ibrahim25@gmail.com>
2023-11-05 06:25:54 +0600

    (backend) fixed username generation bug

--- backend/src/utils/username/generate-unique-username.ts
+++ backend/src/utils/username/generate-unique-username.ts
@@ -2,8 +2,7 @@
 
  ✅ import
 ____________________________________________*/
-import { nanoid } from 'nanoid';
-
+import { customAlphabet } from 'nanoid'
 
 
 /*__________________________________________
@@ -16,35 +15,39 @@ type type_of_generate_unique_username = {
 }
 
 
-
 /*__________________________________________
 
  ✅ util
 ____________________________________________*/
 
-async function generate_unique_username(payload:type_of_generate_unique_username): Promise<string> {
+// Defining a custom nanoid function with the acceptable character set
+const customNanoid = customAlphabet('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_', 8);
 
-  const {full_name, user_model} = payload
+
+async function generate_unique_username(payload: type_of_generate_unique_username): Promise<string> {
+
+  const { full_name, user_model } = payload;
+
+  // Processing the full_name to only include a-zA-Z0-9_, replacing other chars with '_'
+  let sanitized_name = full_name.replace(/[^a-zA-Z0-9_]/g, '_').substring(0, 8)
   
-  let username = `${full_name.trim().replace(/\s+/g, '_').substring(0, 10)}_${nanoid(8)}`;
+  let username = `${sanitized_name}_${customNanoid()}` // Length: Not more than 17 Characters which is within the acceptable range of 20 characters
     
+  
   while (true) {
 
-    const existingUser = await user_model.findOne({ username: username })
+    const existingUser = await user_model.findOne({ username: username });
     
     if (!existingUser) {
        // the username is unique, and we can exit the loop
-      break 
+      break
     }
     
-    // regenerate username
-    username = `${full_name.trim().substring(0, 10)}_${nanoid(8)}`
+    // regenerate username if it's not unique
+    username = `${sanitized_name}_${customNanoid()}`
   }
   
   return username
 }
 
-
-
 export default generate_unique_username
-  
